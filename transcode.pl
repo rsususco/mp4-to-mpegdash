c735ee13b7b766745e1b52ae91c052f22d6a6bd0
zeze <ramil.sususco@informatics.edu.ph>
2017-06-22 17:14:15 +0800

    Update transcode.pl

--- transcode.pl
+++ transcode.pl
@@ -12,13 +12,13 @@ my $config = {
 };
 
 # Pre-defined resolutions
-my $versions = [ '320', '640', '720', '1280', '1920', '2560' ];
+my $versions = [ '320', '640', '720', '1280', '1920', ];
 
 sub create_multiple_bitrate_versions {
 	my ($filename) = @_;
 	my $lastVersion = '';
 	for my $_version (@{$versions}){
-		my $r = `ffmpeg -i $filename -vf scale=$_version:-1:force_original_aspect_ratio=decrease -x264opts 'keyint=$config->{keyint}:min-keyint=$config->{keyint}:no-scenecut' -strict -2 -r $config->{framerate} $_version/$filename -y`;
+		my $r = `ffmpeg -threads 16 -i $filename -preset ultrafast -vf scale=$_version:-1:force_original_aspect_ratio=decrease -x264opts 'keyint=$config->{keyint}:min-keyint=$config->{keyint}:no-scenecut' -strict -2 -r $config->{framerate} $_version/$filename -y`;
 		$lastVersion = $_version;
 	}
 	my $r = `cp $lastVersion/$filename audio/$filename`;
